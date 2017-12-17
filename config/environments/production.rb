1d2558641ab2c7e3c2fb85765f336f63688ff424
Taha2040Qureshi <taha2040qureshi@gmail.com>
2017-12-18 00:59:21 +0500

    add defaut url for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { :host => 'clicknconstruct.heroku.com' }
+
   # Email Configuration for Gmail
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
