3aae3a9e3c47d466989fdbf80936ffd383d3eca4
Taha2040Qureshi <taha2040qureshi@gmail.com>
2017-12-18 11:32:58 +0500

    add some configuration to production for sending email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,6 +54,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.delivery_method = :smtp
   # Email Configuration for Gmail
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
