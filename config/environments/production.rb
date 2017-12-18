5dd40a58cddecc56f14c864e85194ae51e33e000
Taha2040Qureshi <taha2040qureshi@gmail.com>
2017-12-18 10:52:35 +0500

    change protocole to https in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'clicknconstruct.heroku.com' }
+  config.action_mailer.default_url_options = { protocol: 'https', :host => 'clicknconstruct.heroku.com' }
 
   # Email Configuration for Gmail
   config.action_mailer.smtp_settings = {
