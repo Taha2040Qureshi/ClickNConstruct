71d6fd210a276d475e518509ce95da00b7b1ef3a
Taha2040Qureshi <taha2040qureshi@gmail.com>
2017-12-20 15:15:41 +0500

    implementing facebook OmniAuthh with devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   
   root 'home#index'
-  
-  devise_for :users
-
+ 
+  devise_for :users, :controllers => {
+              confirmations: "users/confirmations",
+              omniauth_callbacks: "users/omniauth_callbacks" ,
+              passwords: "users/passwords",
+              registrations: "users/registrations",
+              sessions: "users/sessions"
+            }
 end
